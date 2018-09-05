94998321a4836e7f34b684451035d5f0e5320fc3
amanosalva94 <amanosalva@bytesw.com>
2018-09-05 17:33:38 -0500

    asd asdas

--- webhook.py
+++ webhook.py
@@ -869,12 +869,13 @@ def makeResponse(req):
                             cuentas_tipo_movimiento_monedas = cuentas_json[j]["moneda"]
                             cuentas_tipo_movimiento_dias = formatear_array_fechas(cuentas_json[j]["movimientos_dias"])
                             cuentas_tipo_movimiento_monto = cuentas_json[j]["movimientos_monto"]
+                            cuentas_tipo_movimiento_descripcion = cuentas_json[j]["movimientos_descripcion"] 
                             #for k in range(0,len(cuentas_tipo_movimiento_monto)):
                             for k in range(indice_inicio_pagina,indice_final_pagina):
                                 if float(cuentas_tipo_movimiento_monto[k]) > 0:
-                                    json_string = u'{"title": "' + cuentas_tipo_movimiento_monedas + " " + cuentas_tipo_movimiento_monto[k] + '", "subtitle": "' + cuentas_tipo_movimiento_dias[k] +'","image_url": "https://raw.githubusercontent.com/idusertbs/bytebot-agente-virtual-bancario-webhook/master/bytebot_agente_bancario_assets/plus.png"}'
+                                    json_string = u'{"title": "' + cuentas_tipo_movimiento_monedas + " " + cuentas_tipo_movimiento_monto[k] + '", "subtitle": "' + cuentas_tipo_movimiento_descripcion[k] + cuentas_tipo_movimiento_dias[k] +'","image_url": "https://raw.githubusercontent.com/idusertbs/bytebot-agente-virtual-bancario-webhook/master/bytebot_agente_bancario_assets/plus.png"}'
                                 else:
-                                    json_string = u'{"title": "' + cuentas_tipo_movimiento_monedas + " " + cuentas_tipo_movimiento_monto[k] + '", "subtitle": "' + cuentas_tipo_movimiento_dias[k] +'","image_url": "https://raw.githubusercontent.com/idusertbs/bytebot-agente-virtual-bancario-webhook/master/bytebot_agente_bancario_assets/minus.png"}'
+                                    json_string = u'{"title": "' + cuentas_tipo_movimiento_monedas + " " + cuentas_tipo_movimiento_monto[k] + '", "subtitle": "' + cuentas_tipo_movimiento_descripcion[k] + cuentas_tipo_movimiento_dias[k] +'","image_url": "https://raw.githubusercontent.com/idusertbs/bytebot-agente-virtual-bancario-webhook/master/bytebot_agente_bancario_assets/minus.png"}'
                                     
                                 objeto  = json.loads(json_string,strict=False)
                                 cuentas_tipo_movimiento_array.append(objeto)   
